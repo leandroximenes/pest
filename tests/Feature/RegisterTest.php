e130f357a867554aa8f972a5736ed8cde569c77c
leandro <leandroj.r.ximenes@gmail.com>
2023-08-23 18:16:14 +0000

    done

--- tests/Feature/RegisterTest.php
+++ tests/Feature/RegisterTest.php
@@ -6,22 +6,26 @@ use function Pest\Laravel\post;
 
 uses(RefreshDatabase::class);
 
-it('shows the register page')->get('auth/register')->assertSatatus(200);
+it('shows the register page')->get('auth/register')->assertStatus(200);
 
 it('has errors if the details are not provided')
     ->post('/register')
     ->assertSessionHasErrors(['name', 'email', 'password']);
 
-it('registers the user', function () {
-
-    post('/register', [
-        'name' => 'John',
-        'email' => 'john@example.com',
-        'password' => 'password',
-    ])->assertRedirect('/');
-
-    $this->assertDatabaseHas('users', [
-        'name' => 'John',
-        'email' => 'john@example.com'
-    ])->assertAuthenticated();
-});
+/**
+ * This test covers the complete user registration process.
+ * It first visits the registration page, then submits the form with the required details.
+ * It then asserts that the user is registered, the user is redirected to the home page, and the user is authenticated.
+ *
+ * It uses the tap function to execute a callback within a fluent chain.
+ */
+it('registers the user')
+    ->tap(function () {
+        $this->post('/register', [
+            'name' => 'John',
+            'email' => 'john@example.com',
+            'password' => 'password',
+        ])->assertRedirect('/');
+    })
+    ->assertDatabaseHas('users', ['email' => 'john@example.com'])
+    ->assertAuthenticated();
